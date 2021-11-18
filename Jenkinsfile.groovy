a0b7ba4bee4b61b9452a70e953dd562c450f4fc2
AdiSharK21 <53453464+adishark21@users.noreply.github.com>
2021-11-18 07:59:59 +0530

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    stages {
+      stage ('Stage1') { 
+        steps { 
+          echo "stage 1 print"
+          echo "stage 1 extra print"
+        }
+      }
+      stage ('Stage2') { 
+        steps { 
+          echo "stage 2 print"
+        }
+      }
+      stage ('Stage3') { 
+        steps { 
+          echo "stage 3 print"
+        }
+      }
+    }
+}
